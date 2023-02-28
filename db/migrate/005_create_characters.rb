6eefff582d827917bd176781dfe53827afbaa198
Abdirizak Mohamed <abdirizakm123@gmail.com>
2023-02-28 12:10:03 +0300

    Assignment complete

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    # Your code here phase-3-active-record-associations-tvland-lab
+    create_table :characters do |t|
+      t.string :name 
+      t.integer :actor_id
+      t.integer :show_id
+    end
   end
 end
